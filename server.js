dea5375168435f0fbd54b4534a334bce6ab5c9b7
codesmith <student@mac-mini-5.local>
2016-09-09 12:20:11 -0700

    Added Server Routes

--- server.js
+++ server.js
@@ -3,17 +3,44 @@ var app = express();
 var fs = require('fs');
 var http = require('http');
 
-// write your code here
-// serve the this.html page when /this is visited
-// serve the that.html page when /that is visited
-// DO NOT USE express.static
 
+//static serving html, css, js scrips
+app.use(express.static(__dirname + '/app'));
+
+//all routes tested using postman
 app.get('/test', function(request, response) {
 	response.set({
   	'Content-Type': 'text/html, charset=utf-8',
 	});
 	response.status(200);
 	response.sendFile(__dirname + '/test.html');
+});
+
+app.get('/', function(request, response) {
+response.send("HI I AM GETTING / HERE");
+});
+
+app.post('/', function(request, response) {
+response.send("HI I AM POSTING IN / HERE");
+});
+
+app.get('/home', function(request, response) {
+response.send("HI I AM GETTING IN HOME");
+});
+
+app.post('/create', function(request, response) {
+response.send("HI I AM POSTING CREATE");
+
+});
+
+app.get('/poll', function(request, response) {
+response.send("HI I AM GETTING POLL");
+
+});
+
+app.post('/poll', function(request, response) {
+response.send("I AM POSTING POLL");
+
 });
 
 app.listen(3000);
