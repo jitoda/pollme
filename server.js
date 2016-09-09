86ebb5c956fd7492af52f919ba8f7d58e4be3c24
medic27 <jczhang@ucla.edu>
2016-09-09 14:05:22 -0700

    no change to packagejson - serverjs includes mongoose load

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var http = require('http');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('Connected!!');
+
+	var User = mongoose.model('User', userSchema);
+
+	var test = new User({ username: 'test', password: 'test' });
+	console.log(test.username);
+});
 
 // write your code here
 // serve the this.html page when /this is visited
 // serve the that.html page when /that is visited
 // DO NOT USE express.static
 
-app.get('/test', function(request, response) {
+app.get('/', function(request, response) {
 	response.set({
   	'Content-Type': 'text/html, charset=utf-8',
 	});
